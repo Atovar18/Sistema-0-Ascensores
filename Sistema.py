ca78760f652b8474a1c42bfa7ede88b0ee9d46e4
Humberto Tovar <19-10099@usb.ve>
2023-06-12 09:39:30 -0400

    probando

--- Sistema.py
+++ Sistema.py
@@ -3,10 +3,11 @@ V_n = int (input ("Ingrese el valor de la velocidad nominal del grupo de ascenso
 pn = int (input ("ingrese la capacidad nominal de los ascensores: ")) #capacidad nominal de los ascensores.
 Ar = int (input ("Ingrese el valor del area de ocupacion disponible:"))
 B_i = int (input ("ingrese numero de personas por piso:")) #Numero universal para todos los proyectos.
-B_t= ((B_i * 2 * 30) + (15*3)) #Valor de personas por piso por el factor de ocupacion, sumando los sotanos multiplicado por el numero de personas por sotano.
+B_t= ((B_i * 2 * 45) + (15*3)) #Valor de personas por piso por el factor de ocupacion, sumando los sotanos multiplicado por el numero de personas por sotano.
 print  (B_t , "personas estimadas a utulizar los ascensores.")
-P_v = (32//pn + 0.7*pn + 0.5) #Pasageros por viaje.
-print ( P_v , "personas por viaje")
+P_vd = (3.2//pn + 0.7*pn + 0.5) #Pasageros por viaje.
+print (P_vd , "personas por viaje")
+P_v = int (input ("Inserte el valor entero de la personas por viaje:"))
 n_a = int (input ("introduce el numero de plantas totales, sin la planta principal: ")) #Numero de piso totales.
 #Si se dividen en secciones, tienes que tomar el piso mas alto de la seccion.
 Zot = int (input ("Inserte el numero total de sotanos:"))
@@ -23,22 +24,22 @@ print (H_e , "metro recorridos en expreso y ", H_s, "metros recorridos sobre la 
 n_p = (n*(1-(((n-1)/n)**P_v))) #Numero de paradas probables.
 print (n_p , "paradas probables")
 print ("tomaremos la entrada libre para los pasageros de 1000mm")
-Q = ((H_s// n_p)**0.5) 
+Q = (((H_s// n_p))**0.5) 
 print (Q ,"valor condicional de la velocidad nominal")
 T_1 = float (input ("Ingrese los valores para el tiempo de recorrido, tomando en cosideracion la tabla 7:"))
 T_2 = float (input ("Ingrese los valores para el tiempo de abordaje, tomando en consideracion la tabla 8:"))
-if Q > V_n: #Condiciones para poder calcular el tiempo de viaje.
-    TVC = ((((2*H_a)/(V_n)))-((H_s/V_n))+((2*V_n)/(Q))+(((2*H_s)/((n_p*((((H_s*Q)/n_p)**0.5)))))*(n_p-1)) + (T_1*(n_p+1) + (T_2)*(P_v) ))
+if Q < V_n: #Condiciones para poder calcular el tiempo de viaje.
+    TVC = ((((2*H_a)/(V_n)))-((H_s/V_n))+((2*V_n)/(Q))+(((2*H_s)/((n_p*(Q)))*(n_p-1)) + (T_1*(n_p+1) + (T_2)*(P_v) )))
     print (TVC , "s de viaje sobre las plantas")
 else: 
-    TVC = ((2*H_a)/(V_n))+(((V_n/Q)+T_1)*(n_p+1))-((H_s/(n_p*(V_n)))+((T_2)*(P_v)))
+    TVC = ((2*H_a)/(Q))+(((V_n/1)+T_1)*(n_p+1))-((H_s/(n_p*(V_n)))+((T_2)*(P_v)))
     print (TVC , "s de viaje completo")
-Num = int (input ("Ingrese el valor entero del tiempo de viaje para el calculo del tiempo adicional:"))
+Num = float (input ("Ingrese el valor del tiempo de viaje para el calculo del tiempo adicional:"))
 TA= ((Num*((Zot*10)/100))) #Tiempo que tarda en recorrer los sotanos.
 print (TA, "s de tiempo adicional")
 TTV = float (TA + Num) #Tiempo total.
 print (TTV , "s es el tiempo que tarda en recorrer todo el circuito.")
 I = float (TTV / Z)
 print (I , "s del intervalo probable.") #Intervelo probable, se tiene que cumplir la tabla 1. 
-C = (((((300*P_v)*Z)*100)/(TTV*B))) 
+C = ((((((300*P_v)*Z)*100))/(TTV*B))) 
 print (C , "es su capacidad de transporte.") #Capacidad de transrporte, se tiene que cumplir la tabla 1.
